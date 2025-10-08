24d3e1a9c868bbcd688a5ffd1500d51049867fa2
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-10-08 19:48:44 +0200

    primera subida

---
+++ accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/AccessdadesRa2Ac11Application.java
@@ -0,0 +1,13 @@
+package accesdades.ra2.ac11.accessdades_ra2_ac11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessdadesRa2Ac11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessdadesRa2Ac11Application.class, args);
+	}
+
+}
---
+++ accessdades_ra2_ac11/src/test/java/accesdades/ra2/ac11/accessdades_ra2_ac11/AccessdadesRa2Ac11ApplicationTests.java
@@ -0,0 +1,13 @@
+package accesdades.ra2.ac11.accessdades_ra2_ac11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessdadesRa2Ac11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
