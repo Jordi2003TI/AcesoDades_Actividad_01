a6c2e3f5cf045467f7d3ca13991c6e12f99d78fc
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-10-15 21:38:59 +0200

    casi acabado

--- accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/controller/studentsController.java
+++ accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/controller/studentsController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 import accesdades.ra2.ac11.accessdades_ra2_ac11.model.students;
 import accesdades.ra2.ac11.accessdades_ra2_ac11.repository.StudentsRepository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +22,12 @@ public class studentsController {
     StudentsRepository studentsRepository;
 
     @GetMapping("/students")
-    public String getStudent() {
-        return "Endpoint get";
+    public List<students> getStudent(){
+        return studentsRepository.findAll();
     }
 
     @PostMapping("/students/batch")
-    public String postStudent() {
+    public String addStudent() {
         studentsRepository.save();
         return "Endpoint post";
     }
--- accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/repository/StudentsRepository.java
+++ accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/repository/StudentsRepository.java
@@ -1,9 +1,16 @@
 package accesdades.ra2.ac11.accessdades_ra2_ac11.repository;
 
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import accesdades.ra2.ac11.accessdades_ra2_ac11.model.students;
 
 @Repository
 public class StudentsRepository {
@@ -12,15 +19,27 @@ public class StudentsRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    private static final class StudentsRowMapper implements RowMapper<students>{
+        @Override
+        public students mapRow(ResultSet rs, int rowNum) throws SQLException{
+            students student = new students();
+            student.setId(rs.getLong("id"));
+            student.setNom(rs.getString("nom"));
+            student.setCognom(rs.getString("cognom"));
+            student.setAge(rs.getInt("age"));
+            student.setCicle(rs.getString("cicle"));
+            student.setAny(rs.getInt("`any`"));
+            return student;
+        }
+    }
     
-
-    public void finalAll(){
+    public List<students> findAll(){
         String sql = "SELECT * FROM students";
-        // jdbcTemplate.query(sql, );
+        return jdbcTemplate.query(sql, new StudentsRowMapper());
     }
 
     public void save(){
-        String sql = "insert into students (nom, cognom, age, cicle, `any`) VALUES (?,?,?,?,?)";
+        String sql = "insert into students (nom, cognom, age, cicle, `any`) VALUES (?,?,?,?,?);";
         jdbcTemplate.update(sql, "Jordi", "Masip", 21, "DAM2B", 2003);
     }
 }
