194ddc3b0c95662603601787066760cf5a317dc0
JordiIbanezMasip <jordimasip2003@gmail.com>
2025-10-15 19:58:34 +0200

    subida de clase

--- accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/controller/studentsController.java
+++ accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/controller/studentsController.java
@@ -2,6 +2,11 @@ package accesdades.ra2.ac11.accessdades_ra2_ac11.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import accesdades.ra2.ac11.accessdades_ra2_ac11.model.students;
+import accesdades.ra2.ac11.accessdades_ra2_ac11.repository.StudentsRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -11,16 +16,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequestMapping("/api")
 public class studentsController {
 
-    @GetMapping("students")
+    @Autowired
+    StudentsRepository studentRepository;
+
+    @GetMapping("/students")
     public String getStudent() {
         return "Endpoint get";
     }
 
-    @PostMapping("students/batch")
+    @PostMapping("/students/batch")
     public String postStudent() {
+        studentRepository.save();
         return "Endpoint post";
     }
-    
-    
-
 }
--- accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/repository/StudentsRepository.java
+++ accessdades_ra2_ac11/src/main/java/accesdades/ra2/ac11/accessdades_ra2_ac11/repository/StudentsRepository.java
@@ -1,14 +1,26 @@
 package accesdades.ra2.ac11.accessdades_ra2_ac11.repository;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StudentsRepository {
-    public void finalAll(){
+    // spring boot no detcteda las intancias basicamnete porque no comprende el new asi que usamos el @Autowired para que la instancia
+    
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
+    
+
+    public void finalAll(){
+        //String sql = "SELECT * FROM students";
+        //jdbcTemplate.query(sql, );
     }
 
     public void save(){
-        
+        String sql = "insert into students (nom, cognom, age, cicle, `any` ) VALUES (?,?,?,?,?)";
+        jdbcTemplate.update(sql, "Jordi", "Masip", 21, "DAM2B", 2003);
     }
 }
